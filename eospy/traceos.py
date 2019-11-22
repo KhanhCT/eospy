332a401c1d80410bc26127215f7cdeed4f0f550e
KhanhCT <trongkhanh.hust@gmail.com>
2019-11-22 16:18:12 +0700

    Update API-V2
    Signed-off-by: KhanhCT <trongkhanh.hust@gmail.com>

--- eospy/traceos.py
+++ eospy/traceos.py
@@ -1,7 +1,8 @@
 from .dynamic_url import DynamicUrl
+import requests
 
 class Traceos :
-    def __init__(self, url='https://eos.hyperion.eosrio.io', version='v1') :
+    def __init__(self, url='http://jungle.eossweden.org', version='v2') :
         ''' '''
         self._prod_url = url
         self._version = version
@@ -11,35 +12,32 @@ class Traceos :
         ''' '''
         cmd = eval('self._dynurl.{0}'.format(func))
         url = cmd.create_url()
+        print(url)
         return cmd.get_url(url, **kwargs)
 
     def post(self, func='', **kwargs) :
         ''' '''
         cmd = eval('self._dynurl.{0}'.format(func))
         url = cmd.create_url()
+        print(url)
         return cmd.post_url(url, **kwargs)
 
     def get_transaction(self, trx_id):
         path = 'history.get_transaction'
-        json = {'id': trx_id}
-        return self.post(path, params=None, json=json)
+        params = {'id': trx_id}
+        return self.get(path, params=params)
     
-    def get_actions(self, account_name, options):
+    def get_actions(self, options):
         path = 'history.get_actions'
+        return self.get(path, params=options)
+    
+    def get_account(self, account_name):
+        path = 'state.get_account'
         json_data = {
             "account_name": account_name
         }
-        json_data.update(options)
-        print(json_data)
         return self.post(path, params=None, json=json_data)
     
     def check_heath(self):
         path = 'health'
-        try:
-            result = self.get(path=path)
-            if result.status_code == 200:
-                return True, None
-            return False, response.json()
-        except Exception as ex:
-            err = 'request to {} failed {}'.format(assets_url, e)
-            return False, err
+        return self.get(path, params=None)
